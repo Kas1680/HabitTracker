ce3be732b3a54b1122db8b03e16b7d7b3fed6cf0
Kas1680 <kas1914080@gmail.com>
2022-07-30 19:13:00 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = @"Data Source = Habit.db";
+
+        CreateDatabase();
+
+        void CreateDatabase()
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS yourHabit(
+                        ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+                    tableCmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
